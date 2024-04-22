dda794f83ee3ed8b76ac66bab4f22b7139a4c108
Benjamin Gauvin <m.kaerin@gmail.com>
2024-04-22 19:51:38 +0200

    Scène 3D principale en place

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { OrbitControls } from '@react-three/drei/core/OrbitControls.js';
 import './App.css';
+import { Canvas } from '@react-three/fiber'
+import { PerspectiveCamera } from '@react-three/drei/core/PerspectiveCamera.js';
+import {  Stars } from '@react-three/drei/core/Stars.js';
+import { Suspense } from 'react';
+import MainScene from './components/MainScene.jsx'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='main-scene' >
+      <Canvas >
+        <PerspectiveCamera position={[0, 5, -10]} />
+        <OrbitControls />
+        <Stars />
+        <directionalLight />
+        <ambientLight />
+        <Suspense fallback={null} >
+          <MainScene />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
