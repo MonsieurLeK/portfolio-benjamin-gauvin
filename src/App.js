53ff43f4fb6d4fa7ea93b77e7694de7a39f4548e
Benjamin Gauvin <m.kaerin@gmail.com>
2024-04-23 17:59:13 +0200

    premières cartes cliquables

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import { OrbitControls } from '@react-three/drei/core/OrbitControls.js';
 import './App.css';
 import { Canvas } from '@react-three/fiber'
-import { PerspectiveCamera } from '@react-three/drei/core/PerspectiveCamera.js';
 import {  Stars } from '@react-three/drei/core/Stars.js';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import MainScene from './components/MainScene.jsx'
+import { OrbitControls, PresentationControls, Text } from '@react-three/drei';
+import gsap from 'gsap';
+
 
 
 function App() {
+
+  const [cameraPosition, setCameraPosition] = useState([0, 25, 10])
+
   return (
     <div id='main-scene' >
-      <Canvas >
-        <PerspectiveCamera position={[0, 5, -10]} />
-        <OrbitControls />
+      <Canvas key={cameraPosition.toString()} camera={{position: cameraPosition, fov: 50}} >
+        <OrbitControls minDistance={10} maxDistance={50} enablePan={false} enableRotate={false} />
         <Stars />
         <directionalLight />
         <ambientLight />
+        <PresentationControls cursor={false} 
+        snap={true} 
+        rotation={[-0.3, -0.2, 0]} >
         <Suspense fallback={null} >
           <MainScene />
+          <mesh position={[-5, 5, -5]}
+          rotation={[-0.3, 0.5, 0]} >
+            <boxGeometry args={[3.5, 5, 0.1]} />
+            <meshStandardMaterial color="white" />
+            <Text color="black"
+            scale={0.5}
+            position={[0, 1.1, 0.2]} >{`Apprenez à\n mieux me\n connaître .`}</Text>
+          </mesh>
         </Suspense>
+        </PresentationControls>
       </Canvas>
     </div>
   );
